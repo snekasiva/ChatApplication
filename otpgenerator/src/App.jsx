d64729bda4b000db09bb0c41bea4089d659ec3f6
sneka <sneka1606@gmail.com>
2024-11-25 21:45:26 +0530

    first commit

---
+++ otpgenerator/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import OtpGenerator from './OtpGenerator'
+
+function App() {
+
+  return (
+    <>
+     <OtpGenerator></OtpGenerator>
+    </>
+  )
+}
+
+export default App
---
+++ otpgenerator/src/OtpGenerator.jsx
@@ -0,0 +1,11 @@
+
+function OtpGenerator() {
+  return (
+    <>
+      <h1>OTP Generator</h1>
+      
+    </>
+  )
+}
+
+export default OtpGenerator
